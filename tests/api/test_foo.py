471a330cae147c6f4cb6e2d704b268ba011b5859
Uttam Sharma <u.sharma1@outlook.com>
2024-05-28 10:28:28 +0100

    fix placeholder test

--- tests/api/test_foo.py
+++ tests/api/test_foo.py
@@ -1,10 +1,19 @@
 import unittest
 
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
 
 class TestAPI(unittest.TestCase):
     def test_foo(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        response = client.get("/")
+
+        assert response.status_code == 200
+        assert response.json() == {"Hello": "World"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
